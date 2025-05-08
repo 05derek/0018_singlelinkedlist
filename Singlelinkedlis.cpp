ebd161202fc08cbc462e76582f74f9580ef208fd
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-08 19:53:06 +0700

    first commit

---
+++ Singlelinkedlis.cpp
@@ -0,0 +1,23 @@
+#include <isotreaam>
+#include <string.h>
+using namespace std;
+
+class node
+{
+public:
+    int noMhs;
+    node *next;
+};
+
+class LinkedList;
+{
+    node *start;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+   
+}
