9cada5972f124e15a80af9e5aca4dd62f5c10d74
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-08 20:13:07 +0700

    lanjutan add node

--- Singlelinkedlis.cpp
+++ Singlelinkedlis.cpp
@@ -1,40 +1,16 @@
 
-
-   void addNode()
-   {
-    int nim;
-    cout << "n\Masukkan nomor mahasiswa: ";
-    cin >> nim;
-
-    Node *nodeBaru = new Node;
-    nodebaru->noMhs = nim;
-
-    if (START == NULL || nim <= start->noMhs)
-    {
-        if ((START != NULL) && (nim == START_>noMhs))
+        
+        while ((current != NULL) && (nim >= current->noMhs))
         {
-            cout << "\nDuplikasi noMhs tidak di izinkan\n";
-            return;
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
         }
-        nodeBaru_>nest = START;
-        START = nodeBaru;
-        return;
-    }
-    Node *previous = START;
-    Node *current = START;
 
-    while ((current != NULL) && (nim >= current->noMhs))
-    {
-        if (nim == current->noMhs)
-        {
-            cout << "\nDuplikasi noMhs tidak diizinkan\n";
-            return;
-        }
-        previous = current;
-        current = current->next;
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
-
-    noeBaru->next = current;
-    previous->next = nodeBaru;
-   }
-}
