94aaccbcce135764ce131758fbc772b01a6fe7cf
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-08 20:15:37 +0700

    lanjutan bool search

--- Singlelinkedlis.cpp
+++ Singlelinkedlis.cpp
@@ -1,5 +1,9 @@
 
-    bool Search(int nim, Node **previous, Node **current)
-    {
-        *previous = START;
-        *current = START;
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+    }
